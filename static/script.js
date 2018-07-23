0639c60f3bf829806b5f1bbab403cd7b5bfaaaea
Alexandra Stavrakos <alexandra.stavrakos@gmail.com>
2018-07-23 15:37:14 -0500

    got rid of event listeners for buttons

--- static/script.js
+++ static/script.js
@@ -5,27 +5,27 @@
 //       console.log('click');
 //   });
 // }
-
-document.querySelector('#happy').addEventListener('click', () => {
-    console.log('happy');
-  });
-
-document.querySelector('#sad').addEventListener('click', () => {
-      console.log('sad');
-  });
-
-document.querySelector('#angry').addEventListener('click', () => {
-        console.log('angry');
-    });
-
-document.querySelector('#tired').addEventListener('click', () => {
-      console.log('tired');
-  });
-
-document.querySelector('#calm').addEventListener('click', () => {
-        console.log('calm');
-  });
-
-document.querySelector('#stressed').addEventListener('click', () => {
-        console.log('stressed');
-  });
+// 
+// document.querySelector('#happy').addEventListener('click', () => {
+//     console.log('happy');
+//   });
+//
+// document.querySelector('#sad').addEventListener('click', () => {
+//       console.log('sad');
+//   });
+//
+// document.querySelector('#angry').addEventListener('click', () => {
+//         console.log('angry');
+//     });
+//
+// document.querySelector('#tired').addEventListener('click', () => {
+//       console.log('tired');
+//   });
+//
+// document.querySelector('#calm').addEventListener('click', () => {
+//         console.log('calm');
+//   });
+//
+// document.querySelector('#stressed').addEventListener('click', () => {
+//         console.log('stressed');
+//   });
